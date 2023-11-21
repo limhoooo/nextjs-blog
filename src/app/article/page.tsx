97d792b809065538200311e882961dd2ae916a68
lim ho <dlagh123@gmail.com>
2023-11-21 20:40:07 +0900

    article 헤더 생성

--- src/app/article/page.tsx
+++ src/app/article/page.tsx
@@ -1,9 +1,13 @@
+import SectionHeader from "@/components/text/SectionHeader";
 import React from "react";
 
 export default function Article() {
   return (
     <section>
-      <h2>두고두고 읽으면 좋을 기술 아티클모음</h2>
+      <SectionHeader
+        sectionTitle="Article"
+        descTitle="두고두고 읽으면 좋을 기술 아티클 모음"
+      />
     </section>
   );
 }
